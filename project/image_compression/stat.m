aeec65d4e11000a8ff3f28441a03769656af2851
Pierre Chiang <mingfong231789248@gmail.com>
2023-12-14 18:38:47 +0800

    update stat.m

--- project/image_compression/stat.m
+++ project/image_compression/stat.m
@@ -1,37 +1,33 @@
 % This program plot the relation between the number of selected singular values and the matrix norm
 
 % Input image and define norm type
-image_path = './otter.jpeg';
-norm_type = inf;
-norm_name = 'Infinity norm';
+image_path = './result/result_compression_fruit_orig.png';
+norm_type = 'fro';
+norm_name = 'Frobenius norm';
 [~,img_name,img_ext] = fileparts(image_path);
 img = imread(image_path);
 
 % Get matrix infos
 [m,n,num_layers] = size(img);
 sv_num = min(m,n);
-norms = zeros([1,sv_num,num_layers]);
+errors = zeros(1,sv_num);
 
-% Compute the SVD
-for layer=1:num_layers
-    % Find the SVD of current layer
-    A = double(img(:,:,layer));
-    [U, S, V] = svd(A,"econ");
-    V_conj = V';
-    for rank=1:sv_num
-        result = zeros([m,n]);
-        for i=1:rank
-            result = result + S(i,i) * U(:,i) * V_conj(i,:);
-            norms(1,rank,layer) = norm(A-result, norm_type);
-        end
-        disp(['layer=',num2str(layer),', rank=',num2str(rank)]);
-    end
+% SVD
+orig_norm = norm(double(img), norm_type);
+[U, S, V] = pagesvd(double(img), "econ");
+V_conj = pagetranspose(V);
+
+% Calculate the relative errors
+result = zeros(m,n,num_layers);
+for rank = 1:sv_num
+    fprintf("Calculating rank = %03d...\n", rank);
+    result = result + S(rank,rank,:) .* U(:,rank,:) .* V_conj(rank,:,:);
+    errors(1,rank) = norm(double(result)-double(img), norm_type) / orig_norm;
 end
 
 % Plot the values
 index_vector = 1:min(m,n);
-plot(index_vector, norms(:,:,1), 'r-', index_vector, norms(:,:,2), 'g-', index_vector, norms(:,:,3), 'b-');
-title(['LRA of ',img_name,img_ext]);
+plot(index_vector, errors(:), 'b-');
 xlabel('Approximation Rank');
-ylabel(norm_name);
+ylabel(['Relative Error (', norm_name, ')']);
 grid on;
